aa7b8270a4673a7b21a04943be28f0f894ce5f46
Toanlele <3143246477@qq.com>
2022-12-27 17:10:20 +0800

    第一次提交

--- models/run.go
+++ models/run.go
@@ -1,5 +1,15 @@
 package models
 
-func IndexCreatDev() {
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
+func IndexCreatDev(w http.ResponseWriter, r *http.Request) {
+	t, err := time.Parse(time.RFC3339, "2018-04-06T10:49:05Z")
+	if err != nil {
+		fmt.Println("dome")
+	}
+	w.Write([]byte())
 }
