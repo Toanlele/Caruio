257cfffea0643b77730c2ac8b3f7d3c488ab3c41
Toanlele <3143246477@qq.com>
2022-12-27 17:10:44 +0800

    第一次提交

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"Caruio/routes"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.DOMCar(r)
+	r.HandleFunc("/", dome)
+	log.Fatal(http.ListenAndServe("localhost:2023", r))
+}
+func dome(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("dome")
 }
--- routes/CaruiorRoutes.go
+++ routes/CaruiorRoutes.go
@@ -1,15 +1,16 @@
 package routes
 
 import (
-	"github.com/Toanlele/Caruio/models"
+	"Caruio/models"
+
 	"github.com/gorilla/mux"
 )
 
-type carinfo struct {
-	uid    uint16 //功能号
-	status uint8  //状态
-}
+// type carinfo struct {
+// 	uid    uint16 //功能号
+// 	status uint8  //状态
+// }
 
 var DOMCar = func(router *mux.Router) {
-	router.HandleFunc("/dev", models.IndexCreatDev).Methods("POST")
+	router.HandleFunc("/dev", models.IndexCreatDev).Methods("GET")
 }
